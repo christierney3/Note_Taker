4c7ec155270139a905f2f2411f5aa8915e91f18b
christierney3 <chris.tierney3@gmail.com>
2022-03-16 15:15:54 -0600

    notes now delete, added comments and cleaned up helpers

--- routes/notes.js
+++ routes/notes.js
@@ -4,14 +4,14 @@ const { v4: uuidv4 } = require('uuid');
 const {
     readFromFile,
     readAndAppend,
-    //writeToFile,
+    writeToFile,
   } = require('../helpers/fsUtils');
-
+//Get all notes
 notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
-
-notes.get('/:note_id', (req, res) => {
+// Get note by id
+notes.get('/:id', (req, res) => {
     const noteId = req.params.id;
     readFromFile('./db/db.json')
         .then((data) => JSON.parse(data))
@@ -22,7 +22,19 @@ notes.get('/:note_id', (req, res) => {
             : res.json('No note with that ID');
         });
 })
-
+// Delete note based on id number
+notes.delete('/:id', (req, res) => {
+    const noteId = req.params.id;
+    readFromFile('./db/db.json')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+          // single out note based on id and delete
+        const result = json.filter((note) => note.id !== noteId);
+        writeToFile('./db/db.json', result);
+        res.json(`Note deleted!`);
+      });
+  });
+// Post notes to db.json file
 notes.post('/', (req, res) => {
     console.log(req.body);
 
@@ -32,9 +44,10 @@ notes.post('/', (req, res) => {
         const newNote = {
             title,
             text,
+            // Note id is randomaly generated using uuid
             id: uuidv4(),
         };
-
+        // Append to json file and respond with the newly generated note 
         readAndAppend(newNote, './db/db.json');
         res.json(newNote)        
     } else {
