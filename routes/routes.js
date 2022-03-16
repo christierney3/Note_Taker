d643f3b0cc1e512a86bc50b8fb75c39cb65c54c6
christierney3 <chris.tierney3@gmail.com>
2022-03-16 15:28:44 -0600

    deployed to heroku and added in in listen

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const noteRouter = require('./notes');
 
 const app = express();
-
+// use url '.../notes' for the note.js file
 app.use('/notes', noteRouter);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -18,11 +18,11 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET Route for feedback page
+// GET Route for notes page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT}`)
+  console.log(`App listening at http://localhost:${PORT} || https://note-taker-ct.herokuapp.com/`)
 );
